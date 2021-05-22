391fe1cb01619c674628f113fd1e4337661bdf1b
Programmer101N <namanbaranwal2002@gmail.com>
2021-05-22 12:20:12 +0530

    Changed PORT

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use('/static', express.static('static'));
 
@@ -7,6 +8,6 @@ app.get('/', (req, res) => {
    return res.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(3000, () => {
-	console.log("Local Server is running on localhost:3000");
+app.listen(PORT, () => {
+	console.log("Local Server is running on http://localhost:" + PORT);
 });
